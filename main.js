ddf3445097a260fa86ec99233cc59ebb6433df8c
Sea Sun <sunsealucky@gmail.com>
2023-11-10 20:35:00 +0800

    modify name

--- main.js
+++ main.js
@@ -26,16 +26,9 @@ const min = 5000;
                     response.json = function () {
                         return new Promise((resolve, reject) => {
                             oldJson.apply(this, arguments).then((result) => {
-                                if (CryptoJS.MD5(result.user.name) == 'f88433eaabb9fa424d7295cb353698c8'
-                                    || CryptoJS.MD5(result.user.name) == 'b57061a2924cf89035d3d2438ec0ddf7') {
-                                    result.shixun.name = "鉴于您的过往记录，您无资格使用该脚本，感谢您的安装。"
-                                } else {
-                                    result.game.cost_time = Math.floor(Math.random() * (max - min + 1)) + min;
-                                    result.shixun.forbid_copy = false;
-                                    result.shixun.vip = true;
-                                    result.shixun.name = "FuckEducoder - Powered By SunSeaLucky（已为您设置随机时长）";
-                                    result.user.name += " - 新大第一帅哥";
-                                }
+                                result.game.cost_time = Math.floor(Math.random() * (max - min + 1)) + min;
+                                result.shixun.forbid_copy = false;
+                                result.shixun.vip = true;
                                 resolve(result);
                             });
                         });
@@ -45,6 +38,6 @@ const min = 5000;
             });
         });
     }
-    
+
     window.fetch = hookFetch;
 })();
