7c1b876a13561a54620a87b4396ff5e37ce9c778
Sea Sun <sunsealucky@gmail.com>
2023-11-10 22:29:06 +0800

    cancel random time

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FuckEducoder
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  This scipt is used for unfreezing educoder's copy restriction. Enjoy it!
 // @author       SunSeaLucky
 // @match        https://www.educoder.net/tasks/*
@@ -15,6 +15,10 @@
 const max = 30000;
 const min = 5000;
 
+// 目前设置随机时间的方法出现严重Bug，请谨慎使用！
+// 若仍想快速刷时间，请进入微信头歌小程序，在对应的实例界面左右滑动，可快速刷到int最大值
+setRandomTime = false;
+
 (function () {
     'use strict';
     let oldFetch = fetch;
@@ -27,7 +31,7 @@ const min = 5000;
                         return new Promise((resolve, reject) => {
                             oldJson.apply(this, arguments).then((result) => {
                                 result.game.cost_time = Math.floor(Math.random() * (max - min + 1)) + min;
-                                result.shixun.forbid_copy = false;
+                                if (setRandomTime) result.shixun.forbid_copy = false;
                                 result.shixun.vip = true;
                                 resolve(result);
                             });
