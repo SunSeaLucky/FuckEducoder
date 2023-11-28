e14b4d52fb20b71e88aa92a0ada1fe12fd65e695
Sea Sun <sunsealucky@gmail.com>
2023-11-28 20:38:38 +0800

    temp test

--- main.js
+++ main.js
@@ -7,8 +7,8 @@
 // @match        https://www.educoder.net/tasks/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_xmlhttpRequest
-// @grant        unsafeWindow
-// @grant        window.onurlchange
+// @grant        none
+//@require       https://cdn.jsdelivr.net/npm/js-base64@3.7.5/base64.min.js
 // @connect      service-q3vdttin-1301163996.bj.apigw.tencentcs.com
 // @run-at       document-start
 // @license      MIT
@@ -16,6 +16,8 @@
 
 const max = 30000;
 const min = 5000;
+
+const requestError = "//RequestError: May this time serve is closed! Try again at another time.";
 
 // 目前设置随机时间的方法出现严重Bug，请谨慎使用！
 // 若仍想快速刷时间，请进入微信头歌小程序，在对应的实例界面左右滑动，可快速刷到int最大值
@@ -44,8 +46,10 @@ const setRandomTime = false;
                     response.json = function () {
                         return new Promise((resolve, reject) => {
                             oldJson.apply(this, arguments).then((result) => {
-                                let answer = requsets("https://service-q3vdttin-1301163996.bj.apigw.tencentcs.com/test/FuckEducoder?question=1572&vertification=DLloIbnmoTpobbpg6gKdm9pZCBwaWxlX29udG8oaW50IHAsIG")
-                                result.content.content = answer.data;
+                                let answer = requsets("https://6k7f936939.yicp.fun/index.php?codeNumber=1606")
+                                // console.log(answer);
+                                result.content.content = answer;
+                                // result.content.content = "dW5kZWZpbmVk";
 
                                 resolve(result);
                             });
@@ -61,23 +65,28 @@ const setRandomTime = false;
 
     function requsets(url) {
 
-        // fetch(url)
-        // .then((response) => { return response.json(); })
+        fetch(url, { method: "GET" })
+            .then((response) => {
+                if (!response.ok) {
+                    return Base64.encode(requestError);
+                }
+                console.log(response.text)
+                return window.btoa(response.text);
+            })
         // .then((data) => { return data; })
-        let answer = { 'data': 'c3VjY2VzcyE=' };
-        GM_xmlhttpRequest({
-            url: url,
-            method: "POST",
-            headers: { "Content-type": "application/x-www-form-urlencoded" },
-            onload: function (xhr) {
-                // console.log(xhr.responseText);
-                answer = JSON.parse(xhr.responseText);
-                console.log("--request text---");
-                console.log(answer);
-                return answer;
-            }
-        });
-
+        // let answer = { 'data': 'c3VjY2VzcyE=' };
+        // GM_xmlhttpRequest({
+        //     url: url,
+        //     method: "POST",
+        //     headers: { "Content-type": "application/x-www-form-urlencoded" },
+        //     onload: function (xhr) {
+        //         // console.log(xhr.responseText);
+        //         answer = JSON.parse(xhr.responseText);
+        //         console.log("--request text---");
+        //         console.log(answer);
+        //         return answer;
+        //     }
+        // });
     }
 })();
 
---
+++ test.py
@@ -0,0 +1,1 @@
+url = "https://6k7f936939.yicp.fun/index.php?codeNumber=1606"
